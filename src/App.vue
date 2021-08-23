7c1f9611f2707c4fa71c67546e83f45ddb461fc3
idris agboola <agboolaisholaidreez@gmail.com>
2021-08-23 19:23:42 +0100

    update

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ jobs[0].location }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "./types/Jobs";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      name: "Idris",
+      age: 20 as number,
+    });
+    const jobs = ref<[Job]>([
+      {
+        title: "Software Developer",
+        salary: 2000,
+        location: "Lagos",
+        id: 2,
+      },
+    ]);
+    return {
+      ...toRefs(state),
+      jobs,
+    };
+  },
+});
+</script>
+
+<style></style>
